f0b5054c268a626fe97c2a48a6c1c5673d8f7d44
MyPhuong2612 <161555281+MyPhuong2612@users.noreply.github.com>
2024-04-12 11:34:21 +0700

    rolemodel.js

--- src/api/model/rolemodel.js
+++ src/api/model/rolemodel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const roleSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true }
+    description: { type: String }
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Role = mongoose.model('Role', roleSchema);
 
-module.exports = Product;
+module.exports = Role;

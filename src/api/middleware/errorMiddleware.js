cd7b6703de232abc62f9c7b9778ecc9817d2628d
MyPhuong2612 <161555281+MyPhuong2612@users.noreply.github.com>
2024-04-12 11:31:53 +0700

    errorMiddleware.js

--- src/api/middleware/errorMiddleware.js
+++ src/api/middleware/errorMiddleware.js
@@ -1,6 +1,6 @@
-function errorHandler(err, req, res, next) {
+const errorMiddleware = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: "Internal Server Error" });
-}
+  res.status(500).json({ message: 'Something went wrong!' });
+};
 
-module.exports = errorHandler;
+module.exports = errorMiddleware;

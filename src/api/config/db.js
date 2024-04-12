b57ac7f4d361de35024d1bb7a61f0cea306aabd3
MyPhuong2612 <161555281+MyPhuong2612@users.noreply.github.com>
2024-04-12 11:29:19 +0700

    db.js

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -1,8 +1,10 @@
-module.exports = {
-  // Cấu hình kết nối MongoDB
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/profile_management_db',
-  
-  // Cấu hình JWT
-  jwtSecret: process.env.JWT_SECRET || 'jwtSecretKey',
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d'
-};
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));

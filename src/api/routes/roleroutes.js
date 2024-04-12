cf2c32162ca61f96e52caea51118ab7b8733e58d
MyPhuong2612 <161555281+MyPhuong2612@users.noreply.github.com>
2024-04-12 11:35:07 +0700

    roleroutes.js

--- src/api/routes/roleroutes.js
+++ src/api/routes/roleroutes.js
@@ -1,22 +1,10 @@
-const express = require('express'); 
+// roleRoutes.js
+const express = require('express');
 const router = express.Router();
-const productController = require('../controllers/productController');
-const authMiddleware = require('../middleware/authMiddleware');
-const uploadMiddleware = require('../middleware/uploadMiddleware');
+const roleController = require('../controllers/roleController');
 
-// Route lấy danh sách sản phẩm
-router.get('/', productController.getAllProducts);
-
-// Route lấy thông tin chi tiết sản phẩm
-router.get('/:productId', productController.getProductById);
-
-// Route tạo sản phẩm mới
-router.post('/', authMiddleware.authenticateToken, uploadMiddleware.upload.single('image'), productController.createProduct);
-
-// Route cập nhật thông tin sản phẩm
-router.put('/:productId', authMiddleware.authenticateToken, uploadMiddleware.upload.single('image'), productController.updateProduct);
-
-// Route xóa sản phẩm
-router.delete('/:productId', authMiddleware.authenticateToken, productController.deleteProduct);
+// Define routes
+router.post('/', roleController.createRole);
+router.get('/', roleController.getRoles);
 
 module.exports = router;
